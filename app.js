09144aeee6c5be2587c81ae7b69ca0161bd08cef
Nasruddin Annapuri <nasruddinannapuri@gmail.com>
2024-05-13 14:54:53 +0530

    Add passport for user authentication and connect-flash for displaying messages
    * Installed passport, passport-local, and connect-flash packages
    * Created a new User model for authentication
    * Added passport initialization and session

--- app.js
+++ app.js
@@ -10,6 +10,10 @@ const ExpressError = require("./utils/ExpressError.js");
 const session = require("express-session")
 const flash = require("connect-flash")
 
+const passport = require("passport")
+const LocalStrategy = require("passport-local")
+const User = require("./models/userauth.js");
+
 const listings = require("./routes/listing.js");
 const reviews = require("./routes/review.js")
 // 3)for database
@@ -57,9 +61,17 @@ app.get(
 
 app.use(session(sessionOptions))
 app.use(flash());
-
 // use flash before the routes
  
+// 1 session = 1 individual user so, it comes after the session
+app.use(passport.initialize());
+app.use(passport.session()); 
+passport.use(new LocalStrategy(User.authenticate()))
+
+passport.serializeUser(User.serializeUser());
+// serialize - store user info in to the session
+passport.deserializeUser(User.deserializeUser());
+// deserialize - remove user info from the sesion
 app.use((req, res, next)=>{
   res.locals.success = req.flash("success");
   res.locals.error = req.flash("error");
@@ -91,6 +103,31 @@ app.get("/testListing", async (req, res) => {
 // if above all routes will checked if nothing is
 // there(res not send) then here it will come and
 // execute
+
+
+// demo user- for authentication
+app.get("/demouser", async (req, res)=>{
+  let fakeUser = new User({
+    email: "student@gmail.com",
+    username: "nasruddin"
+  });
+// static method - to store - parameter(User, password, or cb)
+/* 
+register(user, password, cb) - 
+  convenience method to register a new user instance with a given
+  password. Ckeck if username is unique. See login example
+
+*/
+  let registereduser = await User.register(fakeUser, "helloworld");
+  res.send(registereduser);
+})
+
+/*  output:
+{"email":"student@gmail.com","_id":"6641db01f325da8e615d5e8e","username":"nasruddin","salt":"cda187c44482919ce2d7a177aa9fbc78aac180a21d2b23d826800988495e9959","hash":"71e50c8c1f04729ea0a830d07c7304482fa9401071eaa9e92d05fb91855a3220095813376e1b5bb683fee65c21ba0608dcb2d0320b2e0c5c1f09e375b6bf3107254f3141db54e5195600ee621d93d048d186ad87acd7f3aa76e5c941813b56a0a97fac3bd301e6a3e0d4290db258ec6ebc0791a17f8d41f3cb77f831ed7c683714aea1a36824c2b7666f1103ac737e3fcca73f2711d8e15044316d4fac9137e2c001eb581aeb421619908b3b268a8c16bde45e6306281612ed07845cae94e0365457adfb44e5a1d6e0c910ae25bfceedbb8f7e4d1ea9f84d5cc007eda2df04455164f033245eb0263191e278132f5a58bef365ff51a51b422f07f017fcdd065b71fb2e9955db2bebd1ad9aba5a137ddb1a49ac74db673bc82a9ddc3f8d211183b868c479ae524d6806f6ce158fdfeff408243d8573ac937c6f088f93f7a534f16eff8328ff37bcc58e206554c6bad4f89da2bb8a313b1a15dcab54862e56a2659d399ea5abcf5095aa84366a330514be0468b7f145443d1d38f689a9738d7b3978913f5fade713914401961d204bd825f47bc8d7d320febda44b509efb29921adfd7862af3d7f0abdc5666ab3fb44d3116f653d5bf96f2ddac7b4a4a3b88e49928d01bfa19b27d328820a60ec1f8225f7f432f9a9697a83a54cea06b29b3cf21b38b0d7dd27222a507f946f3f39ccbdca5ca2bb60b40f12f7ee72d7f683399b3","__v":0}
+
+pbkdf2 hashing algorithm used
+*/
+
 
 app.use("/listings", listings);
 app.use("/listings/:id/reviews", reviews);
